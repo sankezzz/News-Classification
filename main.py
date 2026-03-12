bf3932013340d495aa0957a35e56c4d3f7e964b1
Sanket Suyawanshi <114396358+sankezzz@users.noreply.github.com>
2026-03-13 02:15:59 +0530

    Final changes : readme and req

--- main.py
+++ main.py
@@ -20,17 +20,17 @@ def run_pipeline():
     else:
         print("\n[Step 1] Processed data already exists.")
 
-    # 2. Feature Engineering
+    # 2.Feature ngineering
     print("\n[Step 2] Extracting Features...")
     X_train, X_test, y_train, y_test = extract_features(
         PROCESSED_TRAIN_PATH, PROCESSED_TEST_PATH, MODEL_DIR
     )
 
-    # 3. Training
+    # 3.Training
     print("\n[Step 3] Training SVM Model...")
     train_model(X_train, y_train, MODEL_DIR)
 
-    # 4. Evaluation (Imports all text & image logic)
+    # 4. Eval
     print("\n[Step 4] Running Evaluation & Generating Visuals...")
     run_full_evaluation(PROCESSED_TEST_PATH)
 
